2659b92850899d05b0e57b1cc5294a222c050669
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-24 09:46:30 +0100

    fix host error

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4i-y)gr65mq0%cbs_!ozhjylwfvc*j+u9)h2cja1v7$=h+a*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
