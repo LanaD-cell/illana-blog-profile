abf308a8e98a6595c1ecfcdc7c86aeeb16babca3
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-24 10:03:31 +0100

    add about page, url and settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4i-y)gr65mq0%cbs_!ozhjylwfvc*j+u9)h2cja1v7$=h+a*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage',
+    'about',
 ]
 
 MIDDLEWARE = [
