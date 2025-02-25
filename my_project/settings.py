eac69c764c9083532d87af4d04a471431b5d1a1c
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-25 13:46:09 +0100

    deployment issue persist, bad request 400 error heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.herokuapp.com', '127.0.0.1', 'illana-blog-profile.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'https://git.heroku.com/illana-blog-profile.git']
 
 
 # Application definition
